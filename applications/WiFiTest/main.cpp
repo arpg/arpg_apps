4b03951fe267e91e4c08c84e9799fedc85c7c395
John Stechschulte <john.l.stechschulte@gmail.com>
2017-07-11 13:52:59 -0600

    WiFi test (and WiFi driver!) working!

--- applications/WiFiTest/main.cpp
+++ applications/WiFiTest/main.cpp
@@ -2,17 +2,16 @@
 #include <HAL/Signal/SignalDevice.h>
 
 void SignalCallback(hal::SignalMsg& _msg) {
-  std::cout << "   WiFi scan result" << std::endl;
+  std::cout << "   WiFi scan result, " << _msg.system_time() << std::endl;
   for(int ii=0; ii<_msg.scan_result_size(); ii++) {
     const hal::SignalScanResult& scan_result = _msg.scan_result(ii);
-    std::cout << scan_result.ssid() << " " << scan_result.bssid()
-      << " " << scan_result.rssi() << std::endl;
+    std::cout << scan_result.bssid() << " " << scan_result.rssi() << std::endl;
   }
 }
 
 int main(int argc, char** argv) {
   // connect to a WiFi scan
-  hal::Signal signal("wifi://en0");
+  hal::Signal signal("wifi:[scanperiod=3]//en0");
   signal.RegisterSignalDataCallback(&SignalCallback);
 
   while(1) {
