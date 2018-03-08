2589a9d1a3b90bfc5f9d1bd1df1b1555b5e84ce6
sinaaghli <sina.aghli@gmail.com>
2018-03-08 14:48:47 -0700

    pretty constant-width output

--- applications/Gamepad/main.cpp
+++ applications/Gamepad/main.cpp
@@ -1,12 +1,16 @@
 #include <HAL/Gamepad.pb.h>
 #include <HAL/Gamepad/GamepadDevice.h>
+#include <iomanip>
 
 void GamepadCallback(hal::GamepadMsg& _msg) {
   std::cout << "Gamepad State -> ";
   for(int ii=0; ii<_msg.num_axes(); ii++) {
+    std::cout << std::fixed << std::setprecision(3) << std::setw(6) << std::setfill(' ');
     std::cout << _msg.axes().data(ii) << ", ";
   }
+  std::cout << std::setw(0);
   std::cout << "| ";
+  std::cout << std::fixed << std::setprecision(0);
   for(int ii=0; ii<_msg.num_buttons(); ii++) {
     std::cout << _msg.buttons().data(ii) << ",";
   }            
